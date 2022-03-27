4f27fd8a34f4af7f7cdd5c6f3b4a96ee7afb60c5
Koblova <alina.kirov@mail.ru>
2022-03-27 22:43:40 +0300

    make tabs

--- js/tabs.js
+++ js/tabs.js
@@ -1,9 +1,30 @@
-const tabButtons = document.querySelectorAll(".design-list__item")
+const tabButtons = document.querySelectorAll(".design-list__item");
+const tabDescriptions = document.querySelectorAll(".design__descr");
+const tabImages = document.querySelectorAll(".design-images");
 
-console.dir(main);
+const changeContent = (array, value) => {
+  array.forEach((elem) => {
+    if (elem.dataset.tabsField === value) {
+      elem.classList.remove("hidden")
+    } else {
+      elem.classList.add("hidden")
+    }
+  })
+}
 
-tabButtons.forEach((tabButton, index) => {
+tabButtons.forEach((tabButton) => {
   tabButton.addEventListener("click", (event) =>{
-    console.log(event.target);
+    const dataValue = tabButton.dataset.tabsHandler;
+
+  changeContent(tabDescriptions, dataValue)
+    changeContent(tabImages, dataValue)
+    
+    tabButtons.forEach((btn) => {
+if (btn === event.target) {
+  btn.classList.add("design-lis__item_active")
+} else {
+  btn.classList.remove("design-lis__item_active")
+}
+    })
   })
 })
